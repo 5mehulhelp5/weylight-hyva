1e82c39af872facc0ae7ea5a852a84871ee2ca6e
Qamar Zaman Butt <qamarbuttx@gmail.com>
2025-05-08 05:08:23 +0500

    Upgraded Magento to the latest version.

--- dev/tests/integration/testsuite/Magento/CatalogInventory/Model/StockItemSave/OnProductUpdate/ByProductModel/ByStockItemTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogInventory/Model/StockItemSave/OnProductUpdate/ByProductModel/ByStockItemTest.php
@@ -58,7 +58,7 @@ class ByStockItemTest extends \PHPUnit\Framework\TestCase
      * Test saving of stock item by product data via product model (deprecated)
      *
      * @magentoDataFixture Magento/Catalog/_files/product_simple.php
-     * @magentoDbIsolation enabled
+     * @magentoDbIsolation disabled
      */
     public function testSave()
     {
